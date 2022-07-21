6b3e9be63a51265392add6b07babfc495dc722cb
Brandon Correa <50252804+brandoncorrea@users.noreply.github.com>
2022-07-21 16:09:38 -0400

    Update deps and create project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject brandoncorrea/clj-deps-example "1.0.0"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojurescript "1.11.4"]]
+  :source-paths ["src/clj" "src/cljc" "src/cljs"]
+  :test-paths ["spec/clj" "spec/cljc" "spec/cljs"])
